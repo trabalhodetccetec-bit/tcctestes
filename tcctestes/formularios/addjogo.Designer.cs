7c0a76ff6ff1b3e93893930b8637a11a62355ad6
miguel <trabalhodetccetec@gmail.com>
2026-03-17 20:55:06 -0300

    slajson

--- tcctestes/formularios/addjogo.Designer.cs
+++ tcctestes/formularios/addjogo.Designer.cs
@@ -36,15 +36,16 @@ namespace tcctestes.formularios
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.jajog = new System.Windows.Forms.RadioButton();
             this.naojog = new System.Windows.Forms.RadioButton();
-            this.jaze = new System.Windows.Forms.RadioButton();
+            this.jajog = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.naoze = new System.Windows.Forms.RadioButton();
+            this.jaze = new System.Windows.Forms.RadioButton();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.adicionar = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -66,23 +67,28 @@ namespace tcctestes.formularios
             // textBox1
             // 
             this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
             this.textBox1.Location = new System.Drawing.Point(186, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(167, 20);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "nome";
+            this.textBox1.Text = "Nome";
             this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // textBox2
             // 
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
             this.textBox2.Location = new System.Drawing.Point(6, 231);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(347, 96);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "descrição";
+            this.textBox2.Text = "Descrição";
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            this.textBox2.MouseLeave += new System.EventHandler(this.textBox2_MouseLeave);
             // 
             // comboBox1
             // 
@@ -96,7 +102,7 @@ namespace tcctestes.formularios
             "Amei"});
             this.comboBox1.Location = new System.Drawing.Point(186, 48);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(74, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // groupBox1
@@ -109,15 +115,16 @@ namespace tcctestes.formularios
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
-            // groupBox2
+            // naojog
             // 
-            this.groupBox2.Controls.Add(this.naoze);
-            this.groupBox2.Controls.Add(this.jaze);
-            this.groupBox2.Location = new System.Drawing.Point(186, 141);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(167, 57);
-            this.groupBox2.TabIndex = 5;
-            this.groupBox2.TabStop = false;
+            this.naojog.AutoSize = true;
+            this.naojog.Location = new System.Drawing.Point(80, 20);
+            this.naojog.Name = "naojog";
+            this.naojog.Size = new System.Drawing.Size(79, 17);
+            this.naojog.TabIndex = 1;
+            this.naojog.TabStop = true;
+            this.naojog.Text = "Não Joguei";
+            this.naojog.UseVisualStyleBackColor = true;
             // 
             // jajog
             // 
@@ -130,27 +137,15 @@ namespace tcctestes.formularios
             this.jajog.Text = "Já joguei";
             this.jajog.UseVisualStyleBackColor = true;
             // 
-            // naojog
+            // groupBox2
             // 
-            this.naojog.AutoSize = true;
-            this.naojog.Location = new System.Drawing.Point(80, 20);
-            this.naojog.Name = "naojog";
-            this.naojog.Size = new System.Drawing.Size(79, 17);
-            this.naojog.TabIndex = 1;
-            this.naojog.TabStop = true;
-            this.naojog.Text = "Não Joguei";
-            this.naojog.UseVisualStyleBackColor = true;
-            // 
-            // jaze
-            // 
-            this.jaze.AutoSize = true;
-            this.jaze.Location = new System.Drawing.Point(7, 20);
-            this.jaze.Name = "jaze";
-            this.jaze.Size = new System.Drawing.Size(61, 17);
-            this.jaze.TabIndex = 0;
-            this.jaze.TabStop = true;
-            this.jaze.Text = "Já zerei";
-            this.jaze.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.naoze);
+            this.groupBox2.Controls.Add(this.jaze);
+            this.groupBox2.Location = new System.Drawing.Point(186, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(167, 57);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
             // 
             // naoze
             // 
@@ -162,6 +157,17 @@ namespace tcctestes.formularios
             this.naoze.TabStop = true;
             this.naoze.Text = "Não zerei";
             this.naoze.UseVisualStyleBackColor = true;
+            // 
+            // jaze
+            // 
+            this.jaze.AutoSize = true;
+            this.jaze.Location = new System.Drawing.Point(7, 20);
+            this.jaze.Name = "jaze";
+            this.jaze.Size = new System.Drawing.Size(61, 17);
+            this.jaze.TabIndex = 0;
+            this.jaze.TabStop = true;
+            this.jaze.Text = "Já zerei";
+            this.jaze.UseVisualStyleBackColor = true;
             // 
             // textBox3
             // 
@@ -183,26 +189,47 @@ namespace tcctestes.formularios
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // adicionar
             // 
-            this.button2.Location = new System.Drawing.Point(6, 333);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(350, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "adicionar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.adicionar.Location = new System.Drawing.Point(6, 333);
+            this.adicionar.Name = "adicionar";
+            this.adicionar.Size = new System.Drawing.Size(350, 23);
+            this.adicionar.TabIndex = 8;
+            this.adicionar.Text = "adicionar";
+            this.adicionar.UseVisualStyleBackColor = true;
+            this.adicionar.Click += new System.EventHandler(this.adicionar_Click);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Categoria",
+            "Aventura",
+            "Ação",
+            "FPS",
+            "Sandbox",
+            "Terror",
+            "MOBA",
+            "Estratégia"});
+            this.comboBox2.Location = new System.Drawing.Point(266, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(87, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
             // addjogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(368, 363);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.adicionar);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.groupBox2);
@@ -238,7 +265,8 @@ namespace tcctestes.formularios
         private System.Windows.Forms.RadioButton jaze;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button adicionar;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
--- tcctestes/formularios/addjogo.cs
+++ tcctestes/formularios/addjogo.cs
@@ -7,20 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
 
 namespace tcctestes.formularios
 {
     public partial class addjogo : Form
     {
+        string cam = "";
         public addjogo()
         {
             InitializeComponent();
-            
+
         }
 
         private void addjogo_Load(object sender, EventArgs e)
         {
             comboBox1.SelectedIndex = 1;
+            comboBox2.SelectedIndex = 0;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -32,13 +36,14 @@ namespace tcctestes.formularios
                 if (opf.ShowDialog() == DialogResult.OK)
                 {
                     pictureBox1.Image = Image.FromFile(opf.FileName);
+                    cam = opf.FileName;
                 }
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try { 
+            try {
                 using (OpenFileDialog opf = new OpenFileDialog())
                 {
                     opf.Filter = "Executaveis|*.exe;*.lnk;*.*";
@@ -49,37 +54,108 @@ namespace tcctestes.formularios
                         textBox1.Text = nome;
                     }
                     textBox3.Text = opf.FileName;
-                
+
                 }
-            }  
+            }
             catch (Exception ex) {
                 MessageBox.Show("Erro: " + ex, "Erro", MessageBoxButtons.OK);
             }
-            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox1_MouseClick(object sender, MouseEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(textBox1.Text)) { }
-            if(textBox1.Text == "nome") { textBox1.Text = ""; }
+            if (textBox1.Text == "Nome") { textBox1.Text = ""; textBox1.ForeColor = Color.Black; }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-           
+
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
             if (string.IsNullOrWhiteSpace(textBox1.Text)) {
-                textBox1.Text = "nome";
+                textBox1.Text = "Nome";
                 textBox1.ForeColor = Color.Gray;
             }
         }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox2.Text)) { }
+            if (textBox2.Text == "Descrição") { textBox2.Text = ""; textBox2.ForeColor = Color.Black; }
+
+        }
+
+        private void textBox2_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox2.Text = "Descrição";
+                textBox2.ForeColor = Color.Gray;
+            }
+        }
+
+        private void adicionar_Click(object sender, EventArgs e)
+        {
+            
+            dados dad = new dados();
+            dad.Nome = textBox1.Text;
+            dad.Descricao = textBox2.Text;
+            dad.pathexe = textBox3.Text;
+            dad.Categoria = comboBox2.SelectedItem.ToString();
+            dad.aval = comboBox1.SelectedItem.ToString();
+            dad.pathimage = cam;
+            if (jajog.Checked) { dad.jogou = jajog.Text; }
+            else { dad.jogou = naojog.Text; }
+            if (jaze.Checked) { dad.zerou = jaze.Text; }
+            else { dad.zerou = naoze.Text; }
+            List<dados> listjog = new List<dados>();
+            string caminho = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dados", "dados.json");
+
+            if (File.Exists(caminho))
+            {
+                string json = File.ReadAllText(caminho);
+
+                if (!string.IsNullOrWhiteSpace(json))
+                {
+                    listjog = JsonSerializer.Deserialize<List<dados>>(json);
+                }
+            }
+
+            listjog.Add(dad);
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            string novoJson = JsonSerializer.Serialize(listjog, options);
+            Directory.CreateDirectory(Path.GetDirectoryName(caminho));
+            File.WriteAllText(caminho, novoJson);
+            MessageBox.Show("Jogo salvo com sucesso!");
+        }
+    }
+    public class dados {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string pathexe { get; set; }
+        public string Categoria { get; set; }
+        public string zerou { get; set; }
+        public string jogou { get; set; }
+        public string pathimage { get; set; }
+        public string aval { get; set; }
+
     }
 }
