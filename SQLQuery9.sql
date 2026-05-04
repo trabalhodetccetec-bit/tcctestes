63c23e3fff80a268562492d4f5a971b2237a7c30
trabalhodetccetec-bit <trabalhodetccetec@gmail.com>
2026-05-04 12:36:56 -0300

    Add files via upload

---
+++ SQLQuery9.sql
@@ -0,0 +1,80 @@
+create table Produto(
+id_prod int primary key identity(1,1),
+nome text,
+descricao text,
+uni int,
+preco float
+);
+
+create table moviment(
+id_mov int primary key identity(1,1),
+id_prod int,
+descricao text,
+tipo char(1) /*default 'E'*/,
+qtd int,
+foreign key (id_prod) references Produto(id_prod)
+);
+
+create table saldo (
+ id_prod int primary key,
+ qtd int,
+ precototal int,
+ foreign key (id_prod) references Produto(id_prod)
+);
+
+alter table saldo alter column precototal float
+/*drop table saldo*/
+	insert into Produto (nome, descricao, uni, preco) values ('Televisão LG 2025 4K UHD', 'televisão da marca LG',213,3580.99)
+	insert into Produto (nome, descricao, uni, preco) values ('Geladeira Brastemp Frost Free', 'geladeira da marca Brastemp', 120, 2899.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Smartphone Samsung Galaxy S24', 'smartphone da marca Samsung', 350, 4599.00);
+	insert into Produto (nome, descricao, uni, preco) values ('Notebook Dell Inspiron 15', 'notebook da marca Dell', 95, 2799.99);
+	insert into Produto (nome, descricao, uni, preco) values ('Micro-ondas Electrolux 30L', 'micro-ondas da marca Electrolux', 180, 899.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Máquina de Lavar Consul 12kg', 'máquina de lavar da marca Consul', 140, 1899.00);
+	insert into Produto (nome, descricao, uni, preco) values ('Ar Condicionado Split LG 12000 BTUs', 'ar condicionado da marca LG', 75, 2199.99);
+	insert into Produto (nome, descricao, uni, preco) values ('Fone de Ouvido 668bt JBL Bluetooth', 'fone da marca JBL', 500, 299.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Caixa de Som Sony 500W', 'caixa de som da marca Sony', 60, 1999.00);
+	insert into Produto (nome, descricao, uni, preco) values ('Monitor Samsung 27 Polegadas QHD', 'monitor da marca Samsung', 130, 1399.99);
+	insert into Produto (nome, descricao, uni, preco) values ('Teclado Mecânico Redragon', 'teclado gamer da marca Redragon', 220, 249.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Mouse Logitech G502', 'mouse gamer da marca Logitech', 300, 199.99);
+	insert into Produto (nome, descricao, uni, preco) values ('Impressora HP DeskJet', 'impressora da marca HP', 110, 599.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Tablet Apple iPad 10ª Geração', 'tablet da marca Apple', 80, 6299.00);
+	insert into Produto (nome, descricao, uni, preco) values ('Smartwatch Xiaomi Mi Band 8', 'smartwatch da marca Xiaomi', 270, 349.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Cafeteira Nespresso Essenza Mini', 'cafeteira da marca Nespresso', 160, 499.99);
+	insert into Produto (nome, descricao, uni, preco) values ('Liquidificador Philips Walita', 'liquidificador da marca Philips', 210, 189.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Ventilador Mondial 40cm', 'ventilador da marca Mondial', 190, 149.90);
+	insert into Produto (nome, descricao, uni, preco) values ('Roteador TP-Link Dual Band', 'roteador da marca TP-Link', 175, 229.90);
+	insert into Produto (nome, descricao, uni, preco) values ('HD Externo Seagate 1TB', 'hd externo da marca Seagate', 145, 549.99);
+	insert into Produto (nome, descricao, uni, preco) values ('PlayStation 5 PRO Sony', 'console da marca Sony', 50, 7999.90);
+
+select * from saldo
+select * from saldo where id_prod = 18;
+insert into moviment (id_prod, descricao, tipo, qtd)
+values (18, 'Saída teste', 's', 2);
+go
+create or alter trigger moviment_saldo
+on moviment
+after insert
+as
+begin
+    set nocount on;
+
+    update s
+    set 
+        s.qtd = s.qtd + 
+            case 
+                when i.tipo = 'E' then i.qtd
+                when i.tipo = 'S' then -i.qtd
+            end,
+        s.precototal = 
+            (s.qtd + 
+                case 
+                    when i.tipo = 'E' then i.qtd
+                    when i.tipo = 'S' then -i.qtd
+                end
+            ) * p.preco
+    from saldo s
+    join inserted i on s.id_prod = i.id_prod
+    join Produto p on p.id_prod = s.id_prod;
+
+end;
+go
