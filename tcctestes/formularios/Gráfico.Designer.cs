efd7823fbe4c0218d3b5ee5ae023c29a4ce038d8
miguel <trabalhodetccetec@gmail.com>
2026-05-18 17:43:44 -0300

    miguel - acho que foi o grafico

---
+++ tcctestes/formularios/Gráfico.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace tcctestes.formularios
+{
+    partial class Gráfico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paginaInicialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verTodosOsJogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Silver;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jogosToolStripMenuItem,
+            this.ajudaToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jogosToolStripMenuItem
+            // 
+            this.jogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paginaInicialToolStripMenuItem,
+            this.verTodosOsJogosToolStripMenuItem});
+            this.jogosToolStripMenuItem.Name = "jogosToolStripMenuItem";
+            this.jogosToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.jogosToolStripMenuItem.Text = "jogos";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.ajudaToolStripMenuItem.Text = "ajuda";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.sobreToolStripMenuItem.Text = "sobre";
+            // 
+            // paginaInicialToolStripMenuItem
+            // 
+            this.paginaInicialToolStripMenuItem.Name = "paginaInicialToolStripMenuItem";
+            this.paginaInicialToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.paginaInicialToolStripMenuItem.Text = "Pagina Inicial";
+            // 
+            // verTodosOsJogosToolStripMenuItem
+            // 
+            this.verTodosOsJogosToolStripMenuItem.Name = "verTodosOsJogosToolStripMenuItem";
+            this.verTodosOsJogosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verTodosOsJogosToolStripMenuItem.Text = "Ver todos os Jogos";
+            // 
+            // Gráfico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Gráfico";
+            this.Text = "Gráfico";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paginaInicialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verTodosOsJogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+    }
+}
