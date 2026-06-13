740a8ce8ee1fe7f180b8d541c29411e2ee5e404e
miguel <trabalhodetccetec@gmail.com>
2026-06-13 19:09:22 -0300

    Miguel - começando a preencher o formulario de feedback

---
+++ tcctestes/SERVICES/EnviarFeedback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace tcctestes.SERVICES
+{
+    class EnviarFeedback
+    {
+        public static void Enviar(string nome)
+        {
+            string webhook = "https://discord.com/api/webhooks/1515073725676916848/b7VwzoJEwPTFhhJp6VBbbu-DZKNsVaBBfpuJ0wUvAyMRTjRMa_gjgYsvZaZ9gamvRSuc";
+            string msg = $"Nome: {nome}";
+            
+        }
+    }
+}
